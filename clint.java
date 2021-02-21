6dbda4bcac2000110fbf96742f74b590d7e2f59d
Rajnish Singh <78335498+rajnish-488@users.noreply.github.com>
2021-02-21 15:50:41 +0530

    Update clint.java

--- clint.java
+++ clint.java
@@ -1,1 +1,27 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
+public class clint{
+  
+  public ststic void main(String[] aegs){
+     try
+     {
+       Scanner scan = new Scanner(System.in);
+       InetAddress ip = InetAddress.getByName("localhost");
+       Socket s = new Socket(ip,9999);
+       
+       
+       DataInputStream datain = new DataInputStream(s.getInputStream()); 
+       DataOutputStream dataout = new DataOutputStream(s.getOutputStream());
+       
+       
+       
+       
+       
+     }
+    
+  }
+  
+  
+}
